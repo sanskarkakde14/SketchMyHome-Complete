67743dfe99a424fc0c8f4389edbbe68acec2ece4
sanskarkakde14 <sanskarkakde13@gmail.com>
2024-07-10 18:18:11 +0530

    reverted

--- dummy/models.py
+++ dummy/models.py
@@ -24,6 +24,7 @@ class UserFile(models.Model):
     dxf_file = models.FileField(upload_to='dxfs/',null=True, blank=True)
     avg_value = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     area_info = models.JSONField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return f"UserFile(user={self.user}, png_image={self.png_image}, dxf_file={self.dxf_file})"
     
